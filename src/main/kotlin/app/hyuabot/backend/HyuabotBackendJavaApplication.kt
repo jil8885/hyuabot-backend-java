35ed6caea294404e8715f83cdcbf43ad6a89b292
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-10-09 14:29:10 +0900

    feat Initial commit

---
+++ src/main/kotlin/app/hyuabot/backend/HyuabotBackendJavaApplication.kt
@@ -0,0 +1,11 @@
+package app.hyuabot.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HyuabotBackendJavaApplication
+
+fun main(args: Array<String>) {
+    runApplication<HyuabotBackendJavaApplication>(*args)
+}
---
+++ src/test/kotlin/app/hyuabot/backend/HyuabotBackendJavaApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.hyuabot.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HyuabotBackendJavaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
